c7130113d1e243f0f02e677d6cbfa93eee45353d
unknown <Colin.creedon@gmail.com>
2015-10-20 16:38:00 -0400

    spooky ghost

---
+++ FourPointOne.java
@@ -0,0 +1,16 @@
+
+                primaryStage.setScene(scene);
+                primaryStage.show();
+        }
+
+        public static void main(String[] args) {
+                launch(args);
+        }
+}ackage FXPackage;
+
+import javax.swing.JOptionPane;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
