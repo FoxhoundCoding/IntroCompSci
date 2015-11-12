a455de204f71998020930351de29e527232a0966
NoFaceNoLife <Colin.creedon@gmail.com>
2015-11-12 15:53:55 -0500

    Create SixPointOneDriver.java

---
+++ SixPointOneDriver.java
@@ -0,0 +1,21 @@
+
+public class SixPointOneDriver {
+
+	public static void main(String[] args) {
+		
+
+		int sum = SixPointOne.sum();
+		double average = SixPointOne.average();
+		int product = SixPointOne.product();
+		int factorial = SixPointOne.factorial();
+		System.out.println(sum);
+		System.out.println(average);
+		System.out.println(product);
+		System.out.println(factorial);
+
+		
+			
+
+	}
+
+}
