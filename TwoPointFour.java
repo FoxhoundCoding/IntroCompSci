3beb9ba06dca0caa1a6a494094f4ee8bd10bcb7a
NoFaceNoLife <Colin.creedon@gmail.com>
2015-11-12 15:51:06 -0500

    Create TwoPointFour.java

---
+++ TwoPointFour.java
@@ -0,0 +1,35 @@
+
+public class TwoPointFour {
+
+	public static void main(String[] args) {
+		String aString = "SPECTRE";
+
+
+		System.out.println(aString.charAt(0));
+
+		System.out.println(	aString.concat(aString));
+
+		System.out.println(aString.equals(aString));
+
+		System.out.println(aString.equalsIgnoreCase(aString));
+
+		System.out.println(aString.indexOf(aString));
+
+		System.out.println(aString.lastIndexOf (aString));
+
+		System.out.println(aString.toLowerCase());
+
+		System.out.println(aString.toUpperCase());
+
+		System.out.println(aString.replace("SP","V"));
+
+		System.out.println(aString.substring(0));
+
+		System.out.println(aString.substring(0,6));
+
+		System.out.println(aString.trim());
+
+
+	}
+
+}
